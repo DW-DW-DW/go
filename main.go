10613846134060b3d503927724380b40bb055239
DW-DW-DW <hillbergsara@gmail.com>
2017-03-25 18:09:45 +0000

    variables and constants section

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
        "fmt"
+       "runtime"
 )
 
+var (
+	dogName string 
+	dogWeight float64
+)
+//Following Go Fundamentals course by Nigel Poulson on Pluralsight
+
 func main () {
-     fmt.Println("Hello world")
+	fmt.Println("Hello world")
+    fmt.Println("Hello", runtime.GOOS)
+
+    getDogInformation()
+}
+
+func getDogInformation () {
+	fmt.Println("Dog's name is ", dogName)
+	fmt.Println("Dog's weight is ", dogWeight)
 }
